bb718fe0956ea2ae9647791dfccc6ea618569b43
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2019-10-21 14:18:06 +0200

    Move sidebar and header to layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,12 +23,133 @@ namespace TrendlogVisualization.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
 
         public IActionResult ElementsDropDowns()
         {
             return View();
         }
+
+        public IActionResult ElementsButtonsStandard()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsIcons()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsBadgesLabels()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsCards()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsListGroup()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsNavigation()
+        {
+            return View();
+        }
+
+        public IActionResult ElementsUtilities()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsTabs()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsAccordions()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsNotifications()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsModals()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsProgressBars()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsTooltipsPopovers()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsCarousel()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsCalender()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsPagination()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsScrollableElements()
+        {
+            return View();
+        }
+
+        public IActionResult ComponentsMaps()
+        {
+            return View();
+        }
+
+        public IActionResult TablesRegular()
+        {
+            return View();
+        }
+
+        public IActionResult DashBoardBoxes()
+        {
+            return View();
+        }
+
+        public IActionResult FormsControls()
+        {
+            return View();
+        }
+
+        public IActionResult FormsLayout()
+        {
+            return View();
+        }
+
+        public IActionResult FormsValidation()
+        {
+            return View();
+        }
+
+        public IActionResult ChartsChartjs()
+        {
+            return View();
+        }
+
     }
 }
